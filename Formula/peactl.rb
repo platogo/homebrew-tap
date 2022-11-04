337e9e0caf84801af3010a9cafef89306aa15f56
Daniils Petrovs <daniils@platogo.com>
2022-11-04 18:24:41 +0100

    Add cask reqs to `peactl` formula

--- Formula/peactl.rb
+++ Formula/peactl.rb
@@ -7,6 +7,8 @@ class Peactl < Formula
 
   def install
     ENV["CGO_ENABLED"] = "0"
+    system "/usr/local/bin/brew", "install", "--cask", "1password/tap/1password-cli"
+    system "/usr/local/bin/brew", "install", "--cask", "1password"
     system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
