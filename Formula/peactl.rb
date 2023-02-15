601844918281cb33f8dfa0db7d61d930cbb4ea9e
Daniils Petrovs <daniils@platogo.com>
2023-02-15 16:41:45 +0100

    Remove required peactl 1Password install steps

--- Formula/peactl.rb
+++ Formula/peactl.rb
@@ -2,15 +2,13 @@ class Peactl < Formula
   desc "Access Platogo Erlang Architecture apps with a single command"
   homepage "https://www.platogo.com"
   head "https://github.com/platogo/peactl.git", branch: "master"
-  version "93eb35c"
+  version "9433de2"
 
   depends_on "go" => :build
   depends_on "dnsmasq"
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "/usr/local/bin/brew", "install", "--cask", "1password/tap/1password-cli"
-    system "/usr/local/bin/brew", "install", "--cask", "1password"
     system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
