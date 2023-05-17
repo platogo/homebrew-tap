ed65a02098ee35b5eae7ac6bc9821bb9227fa2af
Daniils Petrovs <daniils@platogo.com>
2023-05-17 17:45:09 +0200

    Add shell completion installation to steps

--- Formula/peactl.rb
+++ Formula/peactl.rb
@@ -2,14 +2,22 @@ class Peactl < Formula
   desc "Access Platogo Erlang Architecture apps with a single command"
   homepage "https://www.platogo.com"
   head "https://github.com/platogo/peactl.git", branch: "master"
-  version "9433de2"
+  version "0.1.6"
 
   depends_on "go" => :build
-  depends_on "dnsmasq"
 
   def install
     ENV["CGO_ENABLED"] = "0"
     system "go", "build", *std_go_args(ldflags: "-s -w")
+
+    output = Utils.safe_popen_read("#{bin}/peactl", "completion", "zsh")
+    (zsh_completion/"_peactl").write output
+
+    output = Utils.safe_popen_read("#{bin}/peactl", "completion", "bash")
+    (bash_completion/"peactl").write output
+
+    output = Utils.safe_popen_read("#{bin}/peactl", "completion", "fish")
+    (fish_completion/"peactl").write output
   end
 
   test do
